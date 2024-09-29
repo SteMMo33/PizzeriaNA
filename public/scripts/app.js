9cab6bbaadccc72599c8a3b3a176edc0fb6e2b52
SteMMo33 <stefano.mora@libero.it>
2024-09-29 15:31:05 +0200

    Ripartito per problemi ai sorgenti.

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -2,17 +2,19 @@
 var ordine = new Array()
 var listaArticoli = new Array()
 var totaleOrdine = 0
+var nome = ""
+
 
 /*
     Mosta la pagina principale
 */
 function showHome(item)
 {
-    console.log('showHome '+item)
-    document.getElementById('pageContatti').style.display='none';
-    document.getElementById('pageOrdine').style.display='none';
+    console.log('showHome: '+item)
+    document.querySelector('#edtCerca').value=''
+    hidePages()
     document.getElementById('pageHome').style.display='block';
-    getMenuItems(item)
+    if (item) getMenuItems(item)
 }
 
 
@@ -22,98 +24,150 @@ function showHome(item)
 function showContatti()
 {
     console.log('showContatti')
+    hidePages()
     document.getElementById('pageContatti').style.display='block';
-    document.getElementById('pageHome').style.display='none';
-    document.getElementById('pageOrdine').style.display='none';
 }
 
-
-function showOrdine()
+function hidePages()
 {
-    console.log('showOrdine')
-    if (ordine.length==0){
-        M.toast({html: "Nessun elemento in ordine"})
-        return
-    }
-    // Svuota eventuale lista precedente
-    document.querySelectorAll('.addedOrder').forEach( e => e.remove())
-    // Riempie la pagina Ordine
-    var template = document.querySelector('#templateOrdine');
-    var list = document.querySelector('#mainListOrdine');
-    var totaleDiv = document.querySelector('#totaleOrdine');
-
-    totaleOrdine = 0;
-    ordine.forEach( function( val, idx){
-        var newSub = document.querySelector('#templateOrdine').cloneNode(true);
-        newSub.style.display='block'
-        newSub.classList.add('addedOrder')
-        newSub.querySelector('#templateOrdineTitle').textContent = val.nome
-        newSub.querySelector('#templateOrdineNo').textContent = val.qty
-        document.querySelector('#mainListOrdine').appendChild(newSub)
-        totaleOrdine += Number(val.qty)*Number(val.prezzo)
-    })
-    totaleDiv.innerHTML = "Totale ordine: <b>&euro; "+totaleOrdine.toFixed(2)+"</b>"
-
-    // Mostra pagina
-    document.getElementById('pageOrdine').style.display='block';
-    document.getElementById('pageHome').style.display='none';
-}
-
-// Mostra la dlg con la richiesta della quantità
-function showDlgAddToOrder(e){
-
-    console.log("[showDlgAddToOrder]")
-    if (dlgAddToOrder==null){
-        console.error("dlgAddToOrder non definito!")
-        return
-    }
-    var p = e.srcElement.parentElement
-    while(p.tagName!='LI'){
-        p = p.parentElement
-    }
-    var idx = p.getAttribute('idx')
-    var data = listaArticoli[idx]
-    // Aggiorna i dati in dlg
-    document.querySelector('#dlgAddIdx').val = idx
-    document.querySelector('#dlgAddNumero').textContent = "1"
-    document.querySelector('#dlgAddNome').textContent = data.nome
-    // Apre la dlg
-    var dlg = dlgAddToOrder[0]
-    dlg.open()
+    var els = document.querySelectorAll('.page')
+    for (var x = 0; x < els.length; x++)
+        els[x].style.display = 'none';
 }
 
 
-// Mostra la dlg con i settings
-function showDlgSettings(){
+function show(page)
+{
+    console.log("[show] "+page)
+    hidePages()
+    document.getElementById(page).style.display='block';
+}
 
-    console.log("[showDlgSettings]")
-    if (dlgSettings==null){
-        console.error("dlgSettings non definito!")
-        return
-    }
-    // Aggiorna i dati in dlg
-    //document.querySelector('#dlgAddIdx').val = idx
-    document.querySelector('#dlgAddNumero').textContent = "1"
-    //document.querySelector('#dlgAddNome').textContent = data.nome
-    // Apre la dlg
-    //var dlg = dlgAddToOrder[0]
-    dlgSettings.open()
+
+
+/**
+ * Cancella un elemento dell'ordine
+ * @param {} ev 
+ */
+function deleteItem(ev){
+    var idx = dataOrdine = ev.target.getAttribute('item')
+    console.log("> Cancello ", idx)
+    ordine.splice(idx,1)
+    if (ordine.length)
+        showOrdine()
+    else showHome()
 }
 
 
 
 
-function sendEmail(){
-    console.log('[sendEmail]')
-    emailSubject = "Ordine Nuova Aurora"
-    emailBody = "Ordine:\n1 cipolle + origano\n1 vetegale\n\nGrazie\n\n"
-    window.location.href = "mailto:stefano.mora@libero.it?subject=" + emailSubject + "&body=" + emailBody
+// Legge da DB la situazione delle pizze ordinate e aggiorna l'array pizzeOrdinate
+async function contaPizzeOrdinate(){
+    pizzeOrdinate = new Array() // reset
+
+    // Lista ordini
+    var resRef = firebase.firestore().collection("ordini").where("consegnaData","==",dataOrdine)
+    // resRef.get().then( (orderList) => {
+    var orderList = await resRef.get()
+
+    // console.log("orderList: ", orderList)
+    orderList.forEach(
+        function(order){
+            var orderData = order.data()
+
+            console.log(orderData)
+
+            // Deve esserci data ed ora di consegna
+            if (typeof orderData.consegnaData === 'undefined') return
+            if (typeof orderData.consegnaOra === 'undefined') return
+            // if (typeof orderData.consegnaFascia === 'undefined') return
+
+            console.log("consData: ",orderData.consegnaData)
+            console.log("consOra: ",orderData.consegnaOra)
+            //console.log("consFas: ", orderData.consegnaFascia)
+
+            var objOrdine = JSON.parse(orderData.ordine)
+            objOrdine.forEach(
+                function(ord){
+                    if (typeof pizzeOrdinate[orderData.consegnaOra]==='undefined')
+                        pizzeOrdinate[orderData.consegnaOra]=0
+                    if (ord.tipo=='pizze' || ord.tipo=='speciali')
+                        pizzeOrdinate[orderData.consegnaOra] += Number(ord.qty)
+                }
+            )
+        }
+    )
+    console.log("Ordinate: ", pizzeOrdinate)
+}
+
+
+// Mostra la fascia oraria con l'indicazione delle pizze già ordinate
+async function showFascia(){
+    var container = document.querySelector('#gridFascia')
+    var i = 0;
+    var h = new Date()
+    // Orario iniziale
+    h.setHours(18)
+    h.setMinutes(30)
+
+    console.log("QQQ")
+    await contaPizzeOrdinate()
+    console.log("WWW")
+
+    // Cancello le precedenti
+    var hh = document.querySelectorAll(".fascia-item")
+    hh.forEach( (val, key) => val.remove())
+
+    fasciaOrdine = -1
+    while(i < 14){  // Per ogni fascia oraria genera un div
+
+        var hh = h.getHours() + ":" + (("0"+h.getMinutes()).slice(-2))
+
+        // Nuovo elemento
+        var el = document.createElement("div")
+        el.classList.add('collection-item')
+        el.classList.add('fascia-item')
+        el.append(hh)
+
+        if (typeof (pizzeOrdinate[hh]) === 'undefined')
+            pizzeOrdinate[hh] = 0
+
+        // el.append(" ["+pizzeOrdinate[hh]+"]")
+            
+        if (pizzeOrdinate[hh] < 10){
+            el.classList.add('fasciaValida')
+            el.setAttribute('data', hh)
+            el.setAttribute('fascia', i)
+            el.addEventListener("click", fasciaScelta)
+        }
+        else if (pizzeOrdinate[hh] < 15){
+            el.classList.add('fasciaWarn')
+            el.setAttribute('data', hh)
+            el.setAttribute('fascia', i)
+            el.addEventListener("click", fasciaScelta)
+        }
+        else {
+            el.classList.add('fasciaNonValida')
+        }
+        container.append(el)
+
+        // Prossima fascia
+        ++i
+        h.setMinutes( h.getMinutes()+15)
+    }
+
+    // Mostra il giorno scelto
+    document.querySelector('#giornoScelto').textContent = "GIORNO: " +
+        dataOrdine.substring(6,8)+"-"+dataOrdine.substring(4,6)+"-"+dataOrdine.substring(0,4)
+    // Cambio pagina
+    document.getElementById('pageGiorno').style.display='none';
+    document.getElementById('pageFasciaOraria').style.display='block';
 }
 
 
 
 function saveOrder(){
-    console.log('[saveOrder]', ordine)
+    console.log('[+saveOrder]', ordine)
 
     // Controllo nome
     var nome = document.querySelector("#orderName").value;
@@ -124,21 +178,25 @@ function saveOrder(){
     ordine['nome']=nome
 
     var dbOrdini = firebase.firestore().collection("ordini")
-    var id = Date.now().toString(); // dbOrdini.createId();
+    var id = Date.now().toString();
 
     // Scrittura record
     dbOrdini.doc(id).set({
-        ordine: JSON.stringify(ordine), // "Ordine",
+        ordine: JSON.stringify(ordine),
         nome: nome,
 		data: firebase.firestore.FieldValue.serverTimestamp(),
         servito: false,
-        totale: totaleOrdine
+        totale: totaleOrdine,
+        consegnaData: dataOrdine,
+        consegnaOra: orarioOrdine,
+        token: gToken
       })
       .then(
          function(){
             console.log("[saveOrder] OK")
-            M.toast({html: "Ordine inviato correttamente"})
-            setTimeout( function(){ResetOrder(); showHome('pizze')}, 4000);
+            //M.toast({html: "Ordine inviato correttamente"})
+            //setTimeout( function(){ResetOrder(); showHome('pizze')}, 4000);
+            show('pageFinale')
          }
       )
       .catch(
@@ -146,7 +204,7 @@ function saveOrder(){
             console.error("[saveOrder] ", error)
         }
     );
-  console.log('[saveOrder]')
+  console.log('[-saveOrder]')
 }
 
 
@@ -183,7 +241,7 @@ function waitData(submenu, item) {
     return firebase.firestore().collection("menu").doc(submenu).collection(item).get()
 }
 
-function InsertItem(datalist, item){
+function InsertItem(datalist, item, tipo){
     // Titolo pagina
     console.log(item)
     if (item){
@@ -193,7 +251,7 @@ function InsertItem(datalist, item){
         newSub.style.display='block'
         document.querySelector('#mainList').appendChild(newSub);
     }
-
+    // Lista articoli
     datalist.forEach( dataraw => {
         var data = dataraw.data()
         // In Memoria
@@ -202,7 +260,8 @@ function InsertItem(datalist, item){
         var newEl = document.querySelector('#template').cloneNode(true);
         newEl.classList.add('itemAdded')
         newEl.setAttribute('idx', idx.toString())
-        newEl.onclick = showDlgAddToOrder
+        newEl.setAttribute('tipo', tipo)
+        //newEl.onclick = showDlgAddToOrder
         newEl.querySelector('#templateTitle').textContent = data.nome
         newEl.querySelector('#templateDesc').textContent = data.ingredienti
         newEl.querySelector('#templatePrice').innerHTML = "&euro; "+data.prezzo.toFixed(2)
@@ -212,79 +271,107 @@ function InsertItem(datalist, item){
 }
 
 
-
-function getOrdini(item) {
-    console.log("[+getOrdini] "+item)
+function getMenuItems(item) {
+    console.log("[getMenuItems] "+item)
     // Elimina elementi aggiunti precedentemente
     document.querySelectorAll('.itemAdded').forEach( e => e.remove())
 
     try {
-        var orders = {};
-        var nOrdini = 0;
-        var nElementi = 0;
+        var items = {};
 
         // Get Data
-        var resRef = firebase.firestore().collection("ordini");
-        resRef.get().then( async(orderList) => {
+        var resRef = firebase.firestore().collection("menu");
+        resRef.doc(item).get().then( async(itemSnap) => {
+            console.log(itemSnap.id, itemSnap.data())    // Nome sottomenu - es 'Pizze'
+            if (itemSnap.data()==undefined){
+                document.querySelector('#menuTitle').textContent = "Nessun dato"
+                return
+            }
+            // Compone il titolo
+            document.querySelector('#menuIcon').textContent = itemSnap.data().icona
+            document.querySelector('#menuTitle').textContent = itemSnap.data().nome
+            
+            // Lista articoli
+            listaArticoli = new Array()
 
-            // console.log("orderList: ", orderList)    // 
+            // Qeusti hanno un sottomenu
+            if (item=='ristorante'){
+                var r = await waitData("ristorante",'Antipasti');
+                InsertItem( r, "Antipasti")
 
-            orderList.forEach(
-                function(order){
-                    var orderData = order.data();
-                    console.log(orderData)
-                    console.log(orderData.id)
+                r = await waitData("ristorante",'Primi piatti');
+                InsertItem( r, 'Primi piatti')
 
-                    ++nOrdini
+                r = await waitData("ristorante",'Secondi');
+                InsertItem( r, "Secondi piatti")
 
-                    var jsonOrder = orderData.ordine
-                    if(typeof jsonOrder=='undefined')
-                        jsonOrder = "{\"ordine\":[{\"nome\":\"Problema nei dati\",\"qty\":\"-\"}]}"
-                    if(jsonOrder.startsWith("["))
-                        jsonOrder = "{\"ordine\":"+jsonOrder+"}"
-                    console.log("json: ",jsonOrder)
-                    var objOrdine = JSON.parse(jsonOrder)
-                    objOrdine.ordine.forEach(
-                        function(elemento){
-                            console.log(elemento.nome)
+                r = await waitData("ristorante",'Contorni');
+                InsertItem( r, "Contorni")
 
-                            // Duplica l'elemento 'template'
-                            var newEl = document.querySelector('#template').cloneNode(true);
-                            newEl.classList.add('itemAdded')
-                            // newEl.setAttribute('idx', idx.toString())
-                            // newEl.onclick = showDlgAddToOrder
-                            newEl.querySelector('#templateTitle').textContent = elemento.nome
-                            newEl.querySelector('#templateDesc').textContent = orderData.nome + " - " + orderData.data.toDate()
-                            //newEl.querySelector('#templatePrice').innerHTML = "&euro; "+data.prezzo.toFixed(2)
-                            newEl.querySelector('#templatePrice').innerHTML = elemento.qty
+                r = await waitData("ristorante",'Dolci');
+                InsertItem( r, "Dessert")
+            }
+            else if (item=='calzoni'){
+                var r = await waitData('calzoni','calzoni');
+                InsertItem( r, 'Calzoni', 'pizze')
 
-                            newEl.style.display='block'
-                            document.querySelector('#mainList').appendChild(newEl);
+                r = await waitData('calzoni','pizze alte');
+                InsertItem( r, "Pizze alte", 'pizze')
 
-                            var n = Number(elemento.qty)
-                            if (!isNaN(n)) nElementi += n
-                        }
-                    )
-                }
-            )
+            }
+            else if (item=='bevande'){
+                var r = await waitData('bevande','Bevande');
+                InsertItem( r, 'Bevande')
+                var r = await waitData('bevande','Dopo cena');
+                InsertItem( r, 'Dopo cena')
+                var r = await waitData('bevande','Lista dei vini');
+                InsertItem( r, 'Lista dei vini')
+            }
+            else {
+                // SubCollection 'stesso nome'
+                resRef.doc(item).collection(item).get().then( (subitemSnap) => {
+                    var idx = 0
+                    subitemSnap.forEach( (subitem) => {
+                        // console.log(subitem.data()) // Elemento
+                        var data = subitem.data()
+                        listaArticoli.push(data)
 
-            document.querySelector('#menuTitle').textContent = "Ordini: "+nOrdini+" - Elementi: "+nElementi
+                        // Duplica l'elemento 'template'
+                        var newEl = document.querySelector('#template').cloneNode(true);
+                        newEl.classList.add('itemAdded')
+                        newEl.setAttribute('idx', idx.toString())
+                        newEl.setAttribute('tipo', item)
+                        // newEl.onclick = showDlgAddToOrder
+                        newEl.querySelector('#templateTitle').textContent = data.nome
+                        newEl.querySelector('#templateDesc').textContent = data.ingredienti
+                        newEl.querySelector('#templatePrice').innerHTML = "&euro; "+data.prezzo.toFixed(2)
+                        newEl.style.display='block'
+                        document.querySelector('#mainList').appendChild(newEl);
+                        ++idx;
+                    })
+
+                    // console.log(listaArticoli)
+                })
+            }
         })
     } catch (e) {
-        console.error(e)
         return {
             errorMsg: "Something went wrong. Please Try Again."
         }
     }
-    console.log("[-getOrdini]")
+
 }
 
 
 function init() {
-    console.log('init')
-    getOrdini("non")
-    console.log('init end')
+    console.log('+init')
+    getMenuItems("pizze")   // Prima sezione visualizzata
+
+    let tmp = localStorage.getItem('name');
+    if (tmp) document.querySelector('#orderName').value = tmp;
+
+    console.log('-init')
 }
 
-// Inizializzazione indipendete dallo stato del serviceWorker
+// Inizializzazione indipendente dallo stato del serviceWorker
 init();
